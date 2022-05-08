3016295840f39e2adf0aefe9fbcb08f61aa0d722
Michal Diminsky <michal.diminsky@gmail.com>
2022-05-08 11:50:27 +0300

    adding trial figures to the home screen- with npm run build

--- src/scripts/storyLine.js
+++ src/scripts/storyLine.js
@@ -9,7 +9,7 @@ const pFailure = "you failed and a new variant is spreading now, but don't worry
 const pFailureAnon = "You failed and a new variant is spreading now, but don't worry, you can try again and prevent a world catastrophe";
 
 const p = [
-"Vaccinate world's population and help fight the coronavirus disease. If you'll fail, a new variant will arrive...",
+"vaccinate world's population and help fight the coronavirus disease. If you'll fail, a new variant will arrive...",
 "you made it! But the population grew and you need to vaccinate more people now!",
 "you made it again! Now lets see if you can do it even faster!",
 "great job! But bats are camming. Can you vaccinate them too?",
